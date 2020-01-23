de00995e72d2cf9cc62d7692a62408fb321a7559
Zamiul Hasan <zamiulhasan@shopf.co>
2020-01-23 18:13:02 +0600

    used life cycle hook and converted factorial component to functional component

--- src/components/factorial.js
+++ src/components/factorial.js
@@ -1,47 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Factorial extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 0, factorial: 0 };
+export default function Factorial() {
+  const [ givenNumber, setGivenNumber] = useState(0);
+  const [ factorial, setFactorial ] = useState(0);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  factorialize(num) {
+  const factorialize = (num) => {
     if (num < 0) 
           return -1;
     else if (num === 0) 
         return 1;
     else {
-        return (num * this.factorialize(num - 1));
+        return (num * factorialize(num - 1));
     }
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  const handleChange = (e) => {
+    if(e) {
+      e.preventDefault();
+      setGivenNumber(e.target.value);
+    }
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const factorial = this.factorialize(this.state.value);
-    this.setState({ factorial: factorial });
+  const handleSubmit = (e) => {
+    if(e) {
+      e.preventDefault();
+      const number = givenNumber;
+      const factorialValue = factorialize(number);
+      setFactorial(factorialValue);
+    }
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Factorial Calculator</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="number" placeholder="Enter a number..." onChange={this.handleChange} value={this.state.value}/>
-          <br />
-          <button>Calculate Factorial</button>
-        </form>
-        <h2>Factorial: {this.state.factorial}</h2>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Factorial Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="number" placeholder="Enter a number..." value={givenNumber} onChange={handleChange}/>
+        <br />
+        <button>Calculate Factorial</button>
+      </form>
+      <h2>Factorial: { factorial }</h2>
+    </div>
+  );
 }
-
-export default Factorial;
